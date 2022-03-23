7855a80cb6816a2b84d3c106d901319326054a95
YeongSeok Kim <youngstone89@icloud.com>
2022-03-23 14:22:57 +0900

    tested

--- scripts/headless.sh
+++ scripts/headless.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-locust --headless --users 10 --spawn-rate 1 -H http://your-server.com
+locust --headless --users 10 --spawn-rate 1 -H http://localhost:3001/api/v1/minio/events
 
